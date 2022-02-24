6896ef0d6f052cfa4ce930335c9a7e7661b09436
1181881941@qq.com <1181881941@qq.com>
2022-02-24 23:42:47 +0800

    初始化项目

---
+++ mode/src/main/java/com/hope/mode/ModeApplication.java
@@ -0,0 +1,18 @@
+package com.hope.mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+/**
+ * Create by lijin on 2022/2/24 19:25
+ */
+@SpringBootApplication
+public class ModeApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModeApplication.class, args);
+    }
+
+}
---
+++ mode/src/test/java/com/hope/mode/ModeApplicationTests.java
@@ -0,0 +1,19 @@
+package com.hope.mode;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Create by lijin on 2022/2/24 19:37
+ */
+@SpringBootTest
+@Ignore
+@Slf4j
+public class ModeApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
