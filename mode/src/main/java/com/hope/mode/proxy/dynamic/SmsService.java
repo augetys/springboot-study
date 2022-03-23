52ba3be7a2c2787f03ec1efcd4ef54fd58143a0e
augetys <1181881941@qq.com>
2022-03-23 16:10:21 +0800

    Revert "代理模式"
    This reverts commit a37e6b44

--- mode/src/main/java/com/hope/mode/proxy/dynamic/SmsService.java
+++
@@ -1,9 +0,0 @@
-package com.hope.mode.proxy.dynamic;
-
-/**
- * Created by lijin on  2022/3/10
- */
-public interface SmsService {
-    void send();
-    void receive();
-}
--- mode/src/main/java/com/hope/mode/proxy/statics/SmsServiceOne.java
+++
@@ -1,8 +0,0 @@
-package com.hope.mode.proxy.statics;
-
-/**
- * Created by lijin on  2022/3/10
- */
-public interface SmsServiceOne {
-    void send();
-}
