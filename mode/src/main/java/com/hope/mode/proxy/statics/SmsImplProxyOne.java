ae9718319c43285c00543f2b5dfb7cecf96c33d5
augetys <1181881941@qq.com>
2022-03-23 16:22:06 +0800

    代理模式

---
+++ mode/src/main/java/com/hope/mode/proxy/statics/SmsImplProxyOne.java
@@ -0,0 +1,24 @@
+package com.hope.mode.proxy.statics;
+
+/**
+ * Created by lijin on  2022/3/10
+ * 将目标对象注入进代理类，然后在代理类的对应方法调用目标类中的对应方法。
+ * 这样的话，我们就可以通过代理类屏蔽对目标对象的访问，并且可以在目标方法执行前后做一些自己想做的事情。
+ * <p>
+ * 缺点：接口一旦新增加方法，目标对象和代理对象都要进行修改，这是非常麻烦的！针对每个目标类都创建一个代理类
+ */
+public class SmsImplProxyOne implements SmsServiceOne {
+
+    private final SmsServiceOneImpl smsImplService;
+
+    SmsImplProxyOne(SmsServiceOneImpl smsServiceImpl) {
+        this.smsImplService = smsServiceImpl;
+    }
+
+    @Override
+    public void send() {
+        System.out.println("发送短信前");
+        smsImplService.send();
+        System.out.println("发送短信后");
+    }
+}
